2f0ac697262c431affc52bf10956a4016f636814
limeSl <wcdustn@naver.com>
2021-10-20 21:18:16 +0900

    calender start

--- todolist.js
+++ todolist.js
@@ -1,3 +1,11 @@
 function addSchedule(btn){
     alert('일정 추가')
 }
+
+const date = new Date();
+
+const viewYear = date.getFullYear();
+const viewMonth = date.getMonth();
+
+document.querySelector(".year-month").textContent =
+ `${viewYear}년 ${viewMonth + 1}월`;
